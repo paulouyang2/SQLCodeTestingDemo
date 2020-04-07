49d7fc76de91c16ca1dd467fa18fbfac58b471d9
paulouyang2 <31747038+paulouyang2@users.noreply.github.com>
2020-04-06 23:26:20 -0600

    Create SplitString.sql

---
+++ Scripts/Functions/SplitString.sql
@@ -0,0 +1,22 @@
+USE demoDB
+GO
+CREATE FUNCTION dbo.SplitString
+(
+    @List     nvarchar(max),
+    @Delim    nvarchar(255)
+)
+RETURNS TABLE
+AS
+RETURN ( 
+SELECT [value]
+FROM
+    ( 
+    SELECT [value] = LTRIM(RTRIM(SUBSTRING(@List, [Number],
+      CHARINDEX(@Delim, @List + @Delim, [Number]) - [Number])))
+    FROM (SELECT Number = ROW_NUMBER() OVER (ORDER BY name)
+        FROM sys.all_columns) AS x
+    WHERE Number <= LEN(@List)
+        AND SUBSTRING(@Delim + @List, [Number], DATALENGTH(@Delim)/2) = @Delim
+    ) AS y
+);
+GO
