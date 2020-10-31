22876dfef90b5680c18888564ef554c36153194c
paulouyang2 <31747038+paulouyang2@users.noreply.github.com>
2020-10-30 23:18:16 -0600

    Add files via upload

--- Scripts/Procedures/insertDemoTable.sql
+++ Scripts/Procedures/insertDemoTable.sql
@@ -4,10 +4,18 @@ CREATE PROC insertDemoTable(
 @nameList varchar(1000)
 )
 AS
-IF SERVERPROPERTY('ProductMajorVersion') > 11
-    INSERT demoTable
-    SELECT value FROM STRING_SPLIT(@nameList, ',');
-ELSE
-    INSERT demoTable
-    SELECT value FROM dbo.SplitString(@nameList, ',');
+
+-- this will fail 
+
+INSERT demoTable
+SELECT value FROM STRING_SPLIT(@nameList, ',');
+
+-- the fix
+
+-- IF SERVERPROPERTY('ProductMajorVersion') > 11
+--     INSERT demoTable
+--     SELECT value FROM STRING_SPLIT(@nameList, ',');
+-- ELSE
+--     INSERT demoTable
+--     SELECT value FROM dbo.SplitString(@nameList, ',');
 GO
